eeec61c0209ecd186de9b49b3b40b6ad86106f23
pruzanov <pruzanov@oicr.on.ca>
2021-10-27 16:44:46 -0400

    Fixing up RT

--- tests/calculate.sh
+++ tests/calculate.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 cd $1
-for t in *txt;do wc -l $t;done | sed 's! .*/.*!!'
+for t in *fa;do wc -l $t;done | sed 's! .*/.*!!'
+for t in *tsv;do wc -l $t;done | sed 's! .*/.*!!'
+for t in *out;do wc -l $t;done | sed 's! .*/.*!!'
